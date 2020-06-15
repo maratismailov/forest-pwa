35a130e1d4e76874e01c0e85fcd62532ca1ec287
Marat <Ismailov>
2020-06-15 17:43:24 +0600

    Added location and image

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,40 @@
 <script>
-	export let name;
-</script>
+  import { onMount } from "svelte";
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+  let location;
+
+  onMount(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      location = "Geolocation is not supported by this browser.";
+    }
+    console.log(location);
+  });
+
+  const showPosition = () => {
+    location =
+      "Latitude: " +
+      position.coords.latitude +
+      "<br>Longitude: " +
+      position.coords.longitude;
+  };
+
+  const loadFile = e => {
+    var output = document.getElementById('output');
+    output.src = URL.createObjectURL(e.target.files[0]);
+    output.onload = function() {
+      URL.revokeObjectURL(output.src) // free memory
+    }
+  };
+</script>
 
 <style>
 
 </style>
+
+<main>
+  <input type="file" accept="image/*" capture on:change={event => loadFile(event)} />
+  <img id="output" alt="image"/>
+  <div>{location}</div>
+</main>
